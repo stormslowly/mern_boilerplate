9f0b017e664c0c10c33c94c83b0551c33c6f7eed
stormslowly@gmail.com <stormslowly@gmail.com>
2016-05-09 09:07:51 +0800

    use extending componen to write react component

--- shared/components/hello.js
+++ shared/components/hello.js
@@ -1,12 +1,21 @@
 import React from 'react'
-export default function () {
-  return <div>
-    <h1>Hello <strong>MERN</strong></h1>
-    <ul>
-      <li>Mongodb</li>
-      <li>Express</li>
-      <li>React</li>
-      <li>Node.js</li>
-    </ul>
-  </div>
+
+class Hello extends  React.Component{
+  constructor(props,context){
+    super(props,context)
+  }
+
+  render(){
+    return <div>
+      <h1>Hello <strong>X</strong>ERN</h1>
+      <ul>
+        <li>Mongodb</li>
+        <li>Express</li>
+        <li>React</li>
+        <li>Node.js</li>
+      </ul>
+    </div>
+  }
 }
+
+export default Hello
